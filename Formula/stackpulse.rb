e248243d0e8a58da7580b6ebea1af09efb2e2f6f
Or Elimelech <0r3limelech@gmail.com>
2020-10-05 22:08:10 +0300

    wrong path

--- Formula/stackpulse.rb
+++ Formula/stackpulse.rb
@@ -6,7 +6,7 @@ class Stackpulse < Formula
   sha256 "34bac2c3f1fd79b56a1043b2057e2f84c9ec9c5d6e1f64a38a8656ae3681da6c"
 
   def install
-    bin.install "stackpulse"
+    bin.install "bin/stackpulse"
   end
 
   test do
