97091027918d6df5f398673ea79fb9413e819af7
Or Elimelech <0r3limelech@gmail.com>
2020-10-05 22:21:26 +0300

    bottle unneeded to prevent compilation

--- Formula/stackpulse.rb
+++ Formula/stackpulse.rb
@@ -4,6 +4,7 @@ class Stackpulse < Formula
   version "0.5.1"
   url "https://storage.googleapis.com/stackpulse-public/release/latest/darwin/amd64/stackpulse-0.5.1.tar.gz"
   sha256 "34bac2c3f1fd79b56a1043b2057e2f84c9ec9c5d6e1f64a38a8656ae3681da6c"
+  bottle :unneeded
 
   def install
     bin.install "bin/stackpulse"
@@ -13,4 +14,3 @@ class Stackpulse < Formula
     system "#{bin}/stackpulse", "--help"
   end
 end
-
