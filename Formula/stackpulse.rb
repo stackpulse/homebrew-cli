e8af8658154566766188c4cdd45eaef41244f770
Or Elimelech <0r3limelech@gmail.com>
2020-11-02 10:42:48 +0200

    Release 0.5.8

--- Formula/stackpulse.rb
+++ Formula/stackpulse.rb
@@ -1,9 +1,9 @@
 class Stackpulse < Formula
   desc "StackPulse command-line tool"
   homepage "https://stackpulse.com"
-  version "0.5.4"
-  url "https://storage.googleapis.com/stackpulse-public/release/latest/darwin/amd64/stackpulse-0.5.4.tar.gz"
-  sha256 "66c63b8b1e5e62f9829baeeca558e950846403e3277cf5a4c21e840e78a5d911"
+  version "0.5.8"
+  url "https://storage.googleapis.com/stackpulse-public/release/latest/darwin/amd64/stackpulse-0.5.8.tar.gz"
+  sha256 "dbe995b9762d6e38f396082c00bc75d36b10afae1d12be6fff8be51b212d077f"
   bottle :unneeded
 
   def install
