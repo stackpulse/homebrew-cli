a7aec037eb53b7d0afd9bc339a146fb677a313ad
moshebe <moshebe123@gmail.com>
2021-02-10 17:46:30 +0200

    Update stackpulse.rb (#1)
    We can use the version field, I guess it will be less error-prone
    See:
    https://github.com/tclass/homebrew-cloud_sql_proxy/blob/master/cloud_sql_proxy.rb

--- Formula/stackpulse.rb
+++ Formula/stackpulse.rb
@@ -2,7 +2,7 @@ class Stackpulse < Formula
   desc "StackPulse command-line tool"
   homepage "https://stackpulse.com"
   version "0.5.8"
-  url "https://storage.googleapis.com/stackpulse-public/release/latest/darwin/amd64/stackpulse-0.5.8.tar.gz"
+  url "https://storage.googleapis.com/stackpulse-public/release/latest/darwin/amd64/stackpulse-#{version}.tar.gz"
   sha256 "dbe995b9762d6e38f396082c00bc75d36b10afae1d12be6fff8be51b212d077f"
   bottle :unneeded
 
